7b46a19ff9cf383077bc5ac839089a0db4f2110d
arouxel17 <alexandrerouxel17@gmail.com>
2023-02-02 18:40:45 +0100

    mise en place de la page du back office

---
+++ frontend/src/components/back_office/InfoSpot.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import apiConnexion from "@services/apiConnexion";
+
+/* eslint-disable react/prop-types */
+
+function InfoSpot() {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    apiConnexion
+      .get("/spots", spots)
+      .then((data) => {
+        setSpots(data.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="flex flex-row justify-around my-8 mt-10">
+      <table className="table-auto">
+        <thead>
+          <tr className="border-b-2 border-secondary">
+            <th className="text-2xl p-6 px-32">Nom du spot</th>
+            <th className="text-2xl p-6 px-32">Lieu</th>
+            <th className="text-2xl p-6 px-14">Difficulté</th>
+            <th className="text-2xl p-6 px-14">Modifier</th>
+            <th className="text-2xl p-6 px-14">Supprimer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {spots.map((spot) => (
+            <tr>
+              <td className="text-center text-white py-8">{spot.nom}</td>
+              <td className="text-center text-white">{spot.lieu}</td>
+              <td className="text-center text-white">{spot.difficulte}</td>
+              <td className="text-center">
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-8 m-auto"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                  </svg>
+                </button>
+              </td>
+              <td className="text-center">
+                <button type="button">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m6 4.125l2.25 2.25m0 0l2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
+                    />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default InfoSpot;
--- frontend/src/pages/back_office/BackOffice.jsx
+++ frontend/src/pages/back_office/BackOffice.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-
 import Sidebar from "@components/back_office/sidebar";
+import InfoSpot from "@components/back_office/InfoSpot";
 
 function BackOffice() {
+  // const [displayModal, setDisplayModal] = useState(false);
+
   return (
     <div className="flex flex- row bg-primary">
       <Sidebar />
-      <div className="text-white">BackOffice</div>
+      <div className="flex flex-col text-white w-full m-8">
+        <div className="flex flex-row items-center justify-between mx-24">
+          <h1 className="text-2xl font-bold">Résultats ()</h1>
+          <button
+            type="button"
+            className="bg-secondary text-black text-2xl border-2 border-white rounded-xl px-8 py-4 mx-3 hover:bg-white hover:text-black font-bold"
+            // onClick={setDisplayModal}
+          >
+            ADD
+          </button>
+        </div>
+        <div className="">
+          <InfoSpot />
+        </div>
+      </div>
     </div>
   );
 }
