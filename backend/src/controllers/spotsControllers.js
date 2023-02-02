ec825a402b1b61904a6eb0f32887b2b018bc93a1
arouxel17 <alexandrerouxel17@gmail.com>
2023-02-02 15:41:24 +0100

    creation des routes de bases

--- backend/src/controllers/spotsControllers.js
+++ backend/src/controllers/spotsControllers.js
@@ -1,7 +1,7 @@
 const models = require("../models");
 
 const browse = (req, res) => {
-  models.item
+  models.spots
     .findAll()
     .then(([rows]) => {
       res.send(rows);
@@ -13,7 +13,7 @@ const browse = (req, res) => {
 };
 
 const read = (req, res) => {
-  models.item
+  models.spots
     .find(req.params.id)
     .then(([rows]) => {
       if (rows[0] == null) {
@@ -29,14 +29,10 @@ const read = (req, res) => {
 };
 
 const edit = (req, res) => {
-  const item = req.body;
-
-  // TODO validations (length, format...)
-
-  item.id = parseInt(req.params.id, 10);
-
-  models.item
-    .update(item)
+  const spots = req.body;
+  spots.id = parseInt(req.params.id, 10);
+  models.spots
+    .update(spots)
     .then(([result]) => {
       if (result.affectedRows === 0) {
         res.sendStatus(404);
@@ -51,14 +47,11 @@ const edit = (req, res) => {
 };
 
 const add = (req, res) => {
-  const item = req.body;
-
-  // TODO validations (length, format...)
-
-  models.item
-    .insert(item)
+  const spots = req.body;
+  models.spots
+    .insert(spots)
     .then(([result]) => {
-      res.location(`/items/${result.insertId}`).sendStatus(201);
+      res.location(`/spots/${result.insertId}`).sendStatus(201);
     })
     .catch((err) => {
       console.error(err);
@@ -67,7 +60,7 @@ const add = (req, res) => {
 };
 
 const destroy = (req, res) => {
-  models.item
+  models.spots
     .delete(req.params.id)
     .then(([result]) => {
       if (result.affectedRows === 0) {
