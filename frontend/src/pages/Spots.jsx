c082488b536307dc738297dedecab4b4a66d41ee
arouxel17 <alexandrerouxel17@gmail.com>
2023-02-05 10:55:55 +0100

    creation du menu de tri

--- frontend/src/pages/Spots.jsx
+++ frontend/src/pages/Spots.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import apiConnexion from "@services/apiConnexion";
 import NavMini from "@components/NavMini";
@@ -11,6 +11,18 @@ import avatar from "../assets/avatar.png";
 
 function Spots() {
   const [spots, setSpots] = useState([]);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [difficulty, setDifficulty] = useState(searchParams.get("difficulte"));
+  const handleCategory = (dif) => {
+    if (dif) {
+      setSearchParams({ difficulty: dif });
+    } else {
+      setSearchParams();
+    }
+    setDifficulty(dif);
+  };
 
   const fullSpot = () => {
     apiConnexion
@@ -23,7 +35,8 @@ function Spots() {
 
   useEffect(() => {
     fullSpot();
-  }, []);
+    setDifficulty(searchParams.get("difficulte"));
+  }, [searchParams]);
 
   return (
     <div className="flex flex-col text-black">
@@ -44,17 +57,68 @@ function Spots() {
         </div>
       </div>
       <NavComputer />
-      <div className="flex flex-row justify-between mx-10 mb-6 lg:mt-10 lg:mx-32">
+      <div className="flex flex-row justify-between items-center mx-10 mb-6 md:mx-10 lg:mt-10 lg:mx-10">
         <h1 className="text-xl md:text-3xl font-bold">Tous les spots</h1>
-        <h1 className="text-xl md:text-3xl font-bold">Tri</h1>
+        <div className="text-right">
+          <div className="flex flex-row items-center">
+            <button
+              type="button"
+              onClick={() => setShowMenu(!showMenu)}
+              className="text-xl md:text-3xl font-bold flex flex-row items-center"
+            >
+              Tri
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-7 ml-2 md:w-9 md:h-9"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+                />
+              </svg>
+            </button>
+          </div>
+          {showMenu && (
+            <div className="bg-secondary rounded-xl p-6 pl-10 flex flex-col shadow-2xl absolute right-8 mt-2 md:p-20 md:text-2xl">
+              <button
+                type="button"
+                className="my-3 hover:scale-125 font-bold hover:text-primary text-right"
+                onClick={() => handleCategory("debutant")}
+              >
+                Débutant
+              </button>
+              <button
+                type="button"
+                className="my-3 md:my-10 hover:scale-125 font-bold hover:text-primary text-right"
+                onClick={() => handleCategory("intermediaire")}
+              >
+                Intermédiaire
+              </button>
+              <button
+                type="button"
+                className="my-3 hover:scale-125 font-bold hover:text-primary text-right"
+                onClick={() => handleCategory("expert")}
+              >
+                Expert
+              </button>
+            </div>
+          )}
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-24">
         {spots &&
-          spots.map((spot) => (
-            <Link to={`/spots/${spot.id}`}>
-              <Card key={spot.id} spot={spot} />
-            </Link>
-          ))}
+          spots
+            .filter((spot) => spot.difficulte === difficulty || !difficulty)
+            .map((spot) => (
+              <Link to={`/spots/${spot.id}`}>
+                <Card key={spot.id} spot={spot} />
+              </Link>
+            ))}
       </div>
       <Footer />
     </div>
