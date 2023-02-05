321ea43a5197b4345ff9cd01d6505bc1afd9303d
arouxel17 <alexandrerouxel17@gmail.com>
2023-02-05 10:06:38 +0100

    modif mise en page de onespot

--- frontend/src/pages/OneSpot.jsx
+++ frontend/src/pages/OneSpot.jsx
@@ -6,7 +6,6 @@ import NavMini from "@components/NavMini";
 import NavComputer from "@components/NavComputer";
 import Footer from "@components/FooterComputer";
 import logo from "../assets/wave2.png";
-import avatar from "../assets/avatar.png";
 
 function OneSpot() {
   const { id } = useParams();
@@ -36,19 +35,12 @@ function OneSpot() {
       <div className="ml-4 md:ml-36">
         <NavMini />
       </div>
-      <div className="flex flex-row justify-center lg:invisible">
-        <img src={logo} className="w-32 mr-20 md:w-44 md:mr-60" alt="logo2" />
-        <div className="flex flex-row items-center pr-6">
-          <h1 className="mr-4 md:text-3xl">Hello Julien !</h1>
-          <img src={avatar} className="w-12 h-12" alt="avatar" />
-        </div>
-      </div>
       <NavComputer />
-      <div className="flex flex-col lg:flex-row lg:mt-10">
+      <div className="flex flex-col lg:flex-row lg:mt-48">
         <Link to="/spots">
           <button
             type="button"
-            className="flex items-center ml-4 border-2 border-secondary hover:bg-secondary rounded-xl p-2 md:ml-16 absolute"
+            className="flex items-center ml-6 mt-4 border-2 border-primary bg-secondary hover:scale-125 rounded-xl p-2 md:ml-16 absolute md:w-14 md:h-13"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -56,7 +48,7 @@ function OneSpot() {
               viewBox="0 0 24 24"
               strokeWidth="1.5"
               stroke="currentColor"
-              className="w-6 h-6"
+              className="w-6 h-5 md:w-9 md:h-8"
             >
               <path
                 strokeLinecap="round"
@@ -66,12 +58,12 @@ function OneSpot() {
             </svg>
           </button>
         </Link>
-        <div className="flex flex-col items-center justify-center text-xl mb-5">
+        <div className="flex flex-col-reverse lg:flex-col items-center justify-center text-xl mb-5">
           <div className="mt-5 mb-5 md:mb-10 md:text-3xl">{spots.nom} 🌞</div>
           <img
             src={spots.image}
             alt="test"
-            className="rounded-2xl w-11/12 md:w-9/12 lg:w-8/12 shadow-xl"
+            className="lg:rounded-2xl w-full lg:w-8/12 shadow-xl"
           />
         </div>
         <div className="mb-14 md:mx-20 text-xl md:text-2xl lg:m-0 lg:w-11/12 lg:pr-20 lg:mb-0 lg:mt-10">
