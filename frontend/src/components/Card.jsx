4fe083fe44d8fccfd17be831149d46ef1cf421f3
arouxel17 <alexandrerouxel17@gmail.com>
2023-02-05 14:21:43 +0100

    hotfix taille img sur les cards

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function Card({ spot }) {
   return (
     <div className="bg-white m-4 mx-8 rounded-2xl hover:scale-105 shadow-2xl">
-      <img src={spot.image} alt="test" className="rounded-2xl w-full" />
+      <img
+        src={spot.image}
+        alt="test"
+        className="rounded-2xl w-full h-48 md:h-52 lg:h-60"
+      />
       <div className="flex flex-col p-6 text-lg">
         <div>{spot.nom} 🌞</div>
         <div className="flex flex-row justify-between mt-3">
