7d51a2f4450e8c0a43f8e39006bdfcbaf13a78a8
Alexandre <alexandrerouxel17@gmail;com>
2023-08-25 15:20:18 +0200

    mise en place de la connexion selon les roles

--- backend/src/services/users.js
+++ backend/src/services/users.js
@@ -2,8 +2,8 @@ const Joi = require("joi");
 
 const validate = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-    firstname: Joi.string().min(3).max(30).required(),
+    name: Joi.string().min(3).max(30),
+    firstname: Joi.string().min(3).max(30),
     email: Joi.string().email().required(),
     hashedPassword: Joi.string().min(6).max(255).required(),
     role: Joi.string().valid("admin", "user").default("user"),
